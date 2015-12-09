648a8aa284bf7e689b2d4e94e9fd2576e121c142
JackNel <jcneligan@gmail.com>
2015-12-09 00:01:00 -0500

    use pixelmap to draw a sprite instead of import the image.  also example of same sprite rendered twice.

--- core/src/com/codechampions/JacksGame.java
+++ core/src/com/codechampions/JacksGame.java
@@ -21,7 +21,27 @@ public class JacksGame implements ApplicationListener {
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		texture = new Texture(Gdx.files.internal("jet.png"));
+
+		//Create a Pixmap that is 256x128 using 8 bytes for R, G, B, and A
+		pixmap = new Pixmap(256, 128, Pixmap.Format.RGBA8888);
+
+		//Fill pixmap red
+		pixmap.setColor(Color.RED);
+		pixmap.fill();
+
+		//Draw 2 lines forming an X
+		pixmap.setColor(Color.BLACK);
+		pixmap.drawLine(0, 0, pixmap.getWidth()-1, pixmap.getHeight()-1);
+		pixmap.drawLine(0, pixmap.getHeight()-1, pixmap.getWidth()-1, 0);
+
+		//Draw a circle about the middle
+		pixmap.setColor(Color.YELLOW);
+		pixmap.drawCircle(pixmap.getWidth()/2, pixmap.getHeight()/2, pixmap.getHeight()/2-1);
+
+		texture = new Texture(pixmap);
+		//Since we have passed the pixmap to texture, we can dispose of pixmap
+		pixmap.dispose();
+
 		sprite = new Sprite(texture);
 	}
 
@@ -33,10 +53,14 @@ public class JacksGame implements ApplicationListener {
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 1, 1, 1);
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
 		batch.begin();
+		sprite.setPosition(0, 0);
+		sprite.draw(batch);
+		sprite.setPosition(Gdx.graphics.getWidth()/2, Gdx.graphics.getHeight()/2);
+
 		sprite.draw(batch);
 		batch.end();
 	}
