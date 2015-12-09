731b9a92d4403cb4704cb9d3243276d98889c72f
JackNel <jcneligan@gmail.com>
2015-12-08 23:46:04 -0500

    initial import, a lot of html project structure (edit dependencies, edit configurations, import GWT, fix assets folder bug.  Application prints a jet, that's it.

---
+++ html/src/com/codechampions/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.codechampions.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.codechampions.JacksGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(800, 600);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new JacksGame();
+        }
+}
