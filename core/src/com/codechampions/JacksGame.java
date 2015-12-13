abf72785ea2729eb338602fa18afd8578a09ffae
JackNel <jcneligan@gmail.com>
2015-12-13 12:43:19 -0500

    Implemented move left and and right

--- core/src/com/codechampions/JacksGame.java
+++ core/src/com/codechampions/JacksGame.java
@@ -3,6 +3,7 @@ package com.codechampions;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Pixmap;
@@ -12,32 +13,47 @@ import com.badlogic.gdx.utils.Timer;
 
 public class JacksGame implements ApplicationListener {
 	SpriteBatch batch;
-	TextureAtlas textureAtlas;
-	Animation animation;
-	float elapsedTime = 0;
+	Texture texture;
+	Sprite sprite;
 	
 	@Override
 	public void create () {
+		float w = Gdx.graphics.getWidth();
+		float h = Gdx.graphics.getHeight();
 		batch = new SpriteBatch();
-		textureAtlas = new TextureAtlas(Gdx.files.internal("data/spritesheet.atlas"));
-		TextureAtlas.AtlasRegion region = textureAtlas.findRegion("0001");
-		animation = new Animation(1/15f, textureAtlas.getRegions());
+
+		texture = new Texture(Gdx.files.internal("data/0001.png"));
+		sprite = new Sprite(texture);
+		sprite.setPosition(w/2 - sprite.getWidth()/2, h/2 - sprite.getHeight()/2);
 	}
 
 	@Override
 	public void dispose() {
 		batch.dispose();
-		textureAtlas.dispose();
+		texture.dispose();
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+		if(Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+			if(Gdx.input.isKeyPressed(Input.Keys.CONTROL_LEFT))
+				sprite.translateX(-1f);
+			else
+				sprite.translateX(-10.0f);
+		}
+
+		if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+			if(Gdx.input.isKeyPressed(Input.Keys.CONTROL_LEFT))
+				sprite.translateX(1f);
+			else
+				sprite.translateX(10.0f);
+		}
+
 		batch.begin();
-		elapsedTime += Gdx.graphics.getDeltaTime();
-		batch.draw(animation.getKeyFrame(elapsedTime, true), 0, 0);
+		sprite.draw(batch);
 		batch.end();
 	}
 
