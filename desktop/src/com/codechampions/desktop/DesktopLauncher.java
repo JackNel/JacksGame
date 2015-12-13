cf7575a4ddf0c8a844fb4cd1667c77ec53b6017c
JackNel <jcneligan@gmail.com>
2015-12-13 16:20:38 -0500

    Implement orthographic camera, works for desktop, not html

--- desktop/src/com/codechampions/desktop/DesktopLauncher.java
+++ desktop/src/com/codechampions/desktop/DesktopLauncher.java
@@ -7,7 +7,9 @@ import com.codechampions.JacksGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.useGL30 = true;
+		config.title = "camera";
+		config.width = 1280;
+		config.height = 720;
 		new LwjglApplication(new JacksGame(), config);
 
 	}
