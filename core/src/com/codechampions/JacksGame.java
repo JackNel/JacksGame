bb2706ea89602ef1740c93d16f9760bb9d3af0ca
JackNel <jcneligan@gmail.com>
2015-12-16 22:36:19 -0500

    Example of displaying an image using scene2d

--- core/src/com/codechampions/JacksGame.java
+++ core/src/com/codechampions/JacksGame.java
@@ -1,69 +1,42 @@
 package com.codechampions;
 
 import com.badlogic.gdx.*;
-import com.badlogic.gdx.audio.AudioDevice;
-import com.badlogic.gdx.audio.AudioRecorder;
-import com.badlogic.gdx.audio.Music;
-import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.*;
-import com.badlogic.gdx.input.GestureDetector;
-import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.utils.Timer;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Stage;
 
-import java.util.HashMap;
-import java.util.Map;
+public class JacksGame implements ApplicationListener {
 
+	public class MyActor extends Actor {
+		Texture texture = new Texture(Gdx.files.internal("data/jet.png"));
+		@Override
+		public void draw(Batch batch, float alpha) {
+			batch.draw(texture, 0, 0);
+		}
+	}
 
-public class JacksGame implements ApplicationListener, GestureDetector.GestureListener {
-	OrthographicCamera camera;
-	SpriteBatch batch;
-	Texture texture;
-	Sprite sprite;
-
+	private Stage stage;
 
 	@Override
 	public void create() {
-		camera = new OrthographicCamera(1280, 720);
-		batch = new SpriteBatch();
+		stage = new Stage();
 
-		texture = new Texture(Gdx.files.internal("data/Toronto2048wide.jpg"));
-		texture.setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
-
-		sprite = new Sprite(texture);
-		sprite.setOrigin(0, 0);
-		sprite.setPosition(- sprite.getWidth()/2, - sprite.getHeight()/2);
-
-		Gdx.input.setInputProcessor(new GestureDetector(this));
-
-		AudioDevice playbackDevice = Gdx.audio.newAudioDevice(44100, true);
-		AudioRecorder recordingDevice = Gdx.audio.newAudioRecorder(44100, true);
-		short[] samples = new short[44100 * 10]; //10 seconds mono audio
-		recordingDevice.read(samples, 0, samples.length);
-		playbackDevice.writeSamples(samples, 0, samples.length);
-		recordingDevice.dispose();
-		playbackDevice.dispose();
-
+		MyActor myActor = new MyActor();
+		stage.addActor(myActor);
 	}
 
 	@Override
 	public void dispose() {
-		batch.dispose();
-		texture.dispose();
+		stage.dispose();
 
 	}
 
 	@Override
 	public void render() {
-		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
-		batch.setProjectionMatrix(camera.combined);
-		batch.begin();
-		sprite.draw(batch);
-		batch.end();
+		stage.draw();
 	}
 
 	@Override
@@ -77,47 +50,5 @@ public class JacksGame implements ApplicationListener, GestureDetector.GestureLi
 
 	@Override
 	public void resume() {
-	}
-
-	@Override
-	public boolean touchDown(float x, float y, int pointer, int button) {
-		return false;
-	}
-
-	@Override
-	public boolean tap(float x, float y, int count, int button) {
-		return false;
-	}
-
-	@Override
-	public boolean longPress(float x, float y) {
-		return false;
-	}
-
-	@Override
-	public boolean fling(float velocityX, float velocityY, int button) {
-		return false;
-	}
-
-	@Override
-	public boolean pan(float x, float y, float deltaX, float deltaY) {
-		camera.translate(deltaX, 0);
-		camera.update();
-		return false;
-	}
-
-	@Override
-	public boolean panStop(float x, float y, int pointer, int button) {
-		return false;
-	}
-
-	@Override
-	public boolean zoom(float initialDistance, float distance) {
-		return false;
-	}
-
-	@Override
-	public boolean pinch(Vector2 initialPointer1, Vector2 initialPointer2, Vector2 pointer1, Vector2 pointer2) {
-		return false;
 	}
 }
