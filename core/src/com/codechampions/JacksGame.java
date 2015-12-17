d20de7456c607cb7abb12d6513c91ae3aee8997f
JackNel <jcneligan@gmail.com>
2015-12-17 13:58:07 -0500

    Example of actor performing multiple actions at the same time

--- core/src/com/codechampions/JacksGame.java
+++ core/src/com/codechampions/JacksGame.java
@@ -5,35 +5,36 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.*;
 import com.badlogic.gdx.scenes.scene2d.*;
+import com.badlogic.gdx.scenes.scene2d.actions.MoveToAction;
+import com.badlogic.gdx.scenes.scene2d.actions.RotateToAction;
+import com.badlogic.gdx.scenes.scene2d.actions.ScaleToAction;
+
+import java.util.Iterator;
 
 public class JacksGame implements ApplicationListener {
 
 	public class MyActor extends Actor {
 		Texture texture = new Texture(Gdx.files.internal("data/jet.png"));
-		float actorX = 0, actorY = 0;
 		public boolean started = false;
 
 		public MyActor() {
-			setBounds(actorX, actorY, texture.getWidth(), texture.getHeight());  //This tells the InputListener where the actor is located, that way it knows when you clikc on it
-			addListener(new InputListener() {
-				public boolean touchDown (InputEvent event, float x, float y, int pointer, int button) {
-					((MyActor)event.getTarget()).started = true;
-					return true;
-				}
-			});
+			setBounds(getX(), getY(), texture.getWidth(), texture.getHeight());  //This tells the InputListener where the actor is located, that way it knows when you clikc on it
 		}
 
 		@Override
 		public void draw(Batch batch, float alpha) {
-			batch.draw(texture, actorX, actorY);
+			batch.draw(texture, this.getX(), getY(), this.getOriginX(), this.getOriginY(), this.getWidth(),
+					this.getHeight(), this.getScaleX(), this.getScaleY(), this.getRotation(), 0, 0,
+					texture.getWidth(), texture.getHeight(), false, false);
 		}
 
 		@Override
-		public void act(float delta) {  //The act method is similar to update(), it tells the actor(s) what to do, in this case, add 5 time the actor's x location
-			if(started) {
-				actorX += 5;
+		public void act(float delta) {
+			for(Iterator<Action> iter = this.getActions().iterator(); iter.hasNext();) {
+				iter.next().act(delta);
 			}
 		}
+
 	}
 
 	private Stage stage;
@@ -44,7 +45,22 @@ public class JacksGame implements ApplicationListener {
 		Gdx.input.setInputProcessor(stage);
 
 		MyActor myActor = new MyActor();
-		myActor.setTouchable(Touchable.enabled);
+
+		MoveToAction moveAction = new MoveToAction();
+		RotateToAction rotateAction = new RotateToAction();
+		ScaleToAction scaleAction = new ScaleToAction();
+
+		moveAction.setPosition(300f, 0f);  //This is where the jet will move to.
+		moveAction.setDuration(5f); //this is how long it will take to get there
+		rotateAction.setRotation(90f);
+		rotateAction.setDuration(5f);
+		scaleAction.setScale(0.5f);
+		scaleAction.setDuration(5f);
+
+		myActor.addAction(moveAction);  //this assigns the action to the actor
+		myActor.addAction(rotateAction);
+		myActor.addAction(scaleAction);
+
 		stage.addActor(myActor);
 	}
 
